f8366058014348436a43750567f90b9fb4117f24
Jorge Luis <140755201+Jorge-X@users.noreply.github.com>
2024-01-15 19:32:17 -0300

    Update 1app.py

--- codigos_de_workflow/1app.py
+++ codigos_de_workflow/1app.py
@@ -7,10 +7,10 @@ EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 
 # Criando email
 msg = EmailMessage()
-msg['Subject'] = 'AVISO QUE, VOCE FOI AVISADO!'
+msg['Subject'] = 'DAST LACREI'
 msg['From'] = EMAIL_ADDRESS 
 msg['To'] = 'teste1brvs@gmail.com'
-msg.set_content('Olá foi encontrada uma vunerabilidade pelo owasp verifique no https://github.com/Lacrei/lacrei-institucional/actions/workflows/owasp_zap.yml\n ATENCIOSAMENTE: SCAN_OWASP')
+msg.set_content('Olá, tudo bem?!\nJá fez sua verificação de segurança hoje?\n recomendamos fazer uma verificação todos os dias\n Uma falha de segurança é uma grande perigo!\n sempre lembre de anotar as vunerabilidades corrigidas na documentação!\n https://github.com/Lacrei/lacrei-institucional/actions/workflows/owasp_zap.yml\nhttps://www.notion.so/lacrei/anota-es-Vulnerabilidades-e-falso-positivo-9168a0cd41644f4e8e7acba0fc3c8f90  \nATENCIOSAMENTE: SCAN_OWASP')
 
 # Usar a porta 587 com STARTTLS
 with smtplib.SMTP('smtp-mail.outlook.com', 587) as smtp:
